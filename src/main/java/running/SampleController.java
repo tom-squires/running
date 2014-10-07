03f163f9d8cac91c1e3020396a41c0af5493ae65
Tom Squires <tom@tomsquires.com>
2014-10-07 21:31:50 +0100

    Changed request mapping of Hello World controller.

--- src/main/java/running/SampleController.java
+++ src/main/java/running/SampleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableAutoConfiguration
 public class SampleController {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     @ResponseBody
     String home() {
         return "Hello World!";
