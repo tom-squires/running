f7de8459a2c8c611bc96969471b264ca74a0eedc
Tom Squires <tom@tomsquires.com>
2014-10-08 22:06:04 +0100

    Added MongoDB integration.

--- src/main/java/running/Main.java
+++ src/main/java/running/Main.java
@@ -1,25 +1,52 @@
 package running;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
 @Controller
-public class Main {
+public class Main implements CommandLineRunner {
+    @Autowired
+    private CustomerRepository repository;
 
-    @RequestMapping("/")
-    public String home() {
-        return "home";
+    @RequestMapping("/customers")
+    public @ResponseBody List<Customer> customers() {
+        return repository.findAll();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-    
+
+    @Override
+    public void run(String... strings) throws Exception {
+        repository.deleteAll();
+
+        // save a couple of customers
+        repository.save(new Customer("Alice", "Smith"));
+        repository.save(new Customer("Bob", "Smith"));
+
+
+        // fetch an individual customer
+        System.out.println("Customer found with findByFirstName('Alice'):");
+        System.out.println("--------------------------------");
+        System.out.println(repository.findByFirstName("Alice"));
+
+        System.out.println("Customers found with findByLastName('Smith'):");
+        System.out.println("--------------------------------");
+        for (Customer customer : repository.findByLastName("Smith")) {
+            System.out.println(customer);
+        }
+    }
 }
