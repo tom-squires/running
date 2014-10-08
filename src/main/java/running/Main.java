f331d9e27c4696af2b944d4d909c6c8605629504
Tom Squires <tom@tomsquires.com>
2014-10-08 08:47:28 +0100

    Changed default request mapping of main controller to be “/“

--- src/main/java/running/Main.java
+++ src/main/java/running/Main.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Main {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String home() {
         return "home";
     }
